332860948ad324800ddd6c7c8afcf36603328cba
AkiaelNull <akiaelnull@gmail.com>
2022-10-31 19:08:19 -0400

    Input Polling

--- Premake5.lua
+++ Premake5.lua
@@ -1,15 +1,16 @@
 workspace "Shadow"
-    architecture "x64"
+	architecture "x64"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Shadow/vendor/GLFW/include"
 IncludeDir["Glad"] = "Shadow/vendor/Glad/include"
@@ -20,117 +21,117 @@ include "Shadow/vendor/Glad"
 include "Shadow/vendor/imgui"
 
 project "Shadow"
-    location "Shadow"
-    kind "SharedLib"
-    language "C++"
+	location "Shadow"
+	kind "SharedLib"
+	language "C++"
+	staticruntime "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    pchheader "swpch.h"
-    pchsource "Shadow/src/swpch.cpp"
+	pchheader "swpch.h"
+	pchsource "Shadow/src/swpch.cpp"
 
-    files
-    {
-        "%{prj.name}src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}"
-    }
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}"
+	}
 
-    links
-    {
-        "GLFW",
-        "Glad",
-        "ImGui",
-        "opengl32.lib"
-    }
+	links 
+	{ 
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
 
-    filter "system:windows"
-        cppdialect "C++latest"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
 
-        defines
-        {
-            "SW_PLATFORM_WINDOWS",
-            "SW_BUILD_DLL",
-            "GLFW_INCLUDE_NONE"
-        }
+		defines
+		{
+			"SW_PLATFORM_WINDOWS",
+			"SW_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
 
-        postbuildcommands
-        {
-            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-        }
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
 
-    filter "configurations:Debug"
-        defines "SW_DEBUG"
-        buildoptions "/MDd"
-        symbols "On"
+	filter "configurations:Debug"
+		defines "SW_DEBUG"
+		runtime "Debug"
+		symbols "On"
 
-    filter "configurations:Release"
-        defines "SW_RELEASE"
-        buildoptions "/MD"
-        optimize "On"
+	filter "configurations:Release"
+		defines "SW_RELEASE"
+		runtime "Release"
+		optimize "On"
 
-    filter "configurations:Dist"
-        defines "SW_DIST"
-        buildoptions "/MD"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "SW_DIST"
+		runtime "Release"
+		optimize "On"
 
 project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs
-    {
-        "Shadow/vendor/spdlog/include",
-        "Shadow/src"
-    }
+	includedirs
+	{
+		"Shadow/vendor/spdlog/include",
+		"Shadow/src"
+	}
 
-    links
-    {
-        "Shadow"
-    }
+	links
+	{
+		"Shadow"
+	}
 
-    filter "system:windows"
-        cppdialect "C++latest"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
 
-        defines
-        {
-            "SW_PLATFORM_WINDOWS",
-        }
+		defines
+		{
+			"SW_PLATFORM_WINDOWS"
+		}
 
-    filter "configurations:Debug"
-        defines "SW_DEBUG"
-        buildoptions "/MDd"
-        symbols "On"
+	filter "configurations:Debug"
+		defines "SW_DEBUG"
+		runtime "Debug"
+		symbols "On"
 
-    filter "configurations:Release"
-        defines "SW_RELEASE"
-        buildoptions "/MD"
-        optimize "On"
+	filter "configurations:Release"
+		defines "SW_RELEASE"
+		runtime "Release"
+		optimize "On"
 
-    filter "configurations:Dist"
-        defines "SW_DIST"
-        buildoptions "/MD"
-        optimize "On"
+	filter "configurations:Dist"
+		defines "SW_DIST"
+		runtime "Release"
+		optimize "On"
--- Shadow/src/Shadow/Application.cpp
+++ Shadow/src/Shadow/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Shadow {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -59,6 +61,9 @@ namespace Shadow {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			SW_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
