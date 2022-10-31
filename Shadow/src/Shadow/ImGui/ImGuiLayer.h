b1e1f659d9eabe0268ac4f9b6c71e095f3461709
AkiaelNull <akiaelnull@gmail.com>
2022-10-31 17:50:14 -0400

    ImGui Events

--- Shadow/src/Shadow/ImGui/ImGuiLayer.h
+++ Shadow/src/Shadow/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Shadow/Layer.h"
 
+#include "Shadow/Events/ApplicationEvent.h"
+#include "Shadow/Events/KeyEvent.h"
+#include "Shadow/Events/MouseEvent.h"
+
 namespace Shadow {
 
 	class SW_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Shadow {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
